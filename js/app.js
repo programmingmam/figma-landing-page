72b4b058af855a52604a276f39ed354648cc01ef
Mamun Rana <117065526+programmingmam@users.noreply.github.com>
2023-03-14 11:27:53 -0700

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
- 
+
+// product slider
+
+const sliderContainer = document.querySelector('.slider-container');
+const prevBtn = document.querySelector('.prev-btn');
+const nextBtn = document.querySelector('.next-btn');
+const productCards = document.querySelectorAll('.product-card');
+
+let slideIndex = 0;
+const slideWidth = productCards[0].offsetWidth + 40;
+
+prevBtn.addEventListener('click', () => {
+  slideIndex--;
+  if (slideIndex < 0) {
+    slideIndex = productCards.length - 1;
+  }
+  sliderContainer.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+nextBtn.addEventListener('click', () => {
+  slideIndex++;
+  if (slideIndex > productCards.length - 1) {
+    slideIndex = 0;
+  }
+  sliderContainer.style.transform = `translateX(-${slideIndex * slideWidth}px)`;
+});
+
+// end
